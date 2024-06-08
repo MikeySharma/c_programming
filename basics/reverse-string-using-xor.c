4df1e9a96c827273156c37ba09c3a6e1471c96d1
Mikey Sharma <mikeysharma99@gmail.com>
2024-06-08 21:26:10 +0545

    added reverse-string-using-xor

--- basics/reverse-string-using-xor.c
+++ basics/reverse-string-using-xor.c
@@ -8,12 +8,11 @@ int main()
     char str[100];
     printf("Enter your string: ");
     fgets(str, sizeof(str), stdin);
-    // Remove newline character if present
+    // remove new line if present
     str[strcspn(str, "\n")] = '\0';
-
     reverseString(str);
-    printf("Reversed string is: %s\n", str);
 
+    printf("Reversed String is %s. \n", str);
     return 0;
 }
 
@@ -21,11 +20,10 @@ void reverseString(char *str)
 {
     int length = strlen(str);
     int i, j;
-    char temp;
     for (i = 0, j = length - 1; i < j; i++, j--)
     {
-        temp = str[i];
-        str[i] = str[j];
-        str[j] = temp;
+        str[i] ^= str[j];
+        str[j] ^= str[i];
+        str[i] ^= str[j];
     }
 }
